a6925da2360c5217e6792419637db1fe6dae299d
Robert Massaioli <robertmassaioli@gmail.com>
2018-04-04 14:34:31 +1000

    Support react-sticky 6.0.2
    I have only made additive changes to this type definition so it should be backwards compatible with 5.0 if you simply don't use the new fields.

--- types/react-sticky/index.d.ts
+++ types/react-sticky/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for react-sticky 5.0
+// Type definitions for react-sticky 6.0.2
 // Project: https://github.com/captivationsoftware/react-sticky
-// Definitions by: Matej Lednicky <http://www.thinkcreatix.com/>, Curtis Warren <https://github.com/curtisw0>
+// Definitions by: Matej Lednicky <http://www.thinkcreatix.com/>, Curtis Warren <https://github.com/curtisw0>, Robert Massaioli <https://github.com/robertmassaioli>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.6
 
@@ -9,6 +9,7 @@ import * as React from "react";
 export const StickyContainer: React.ComponentClass<React.HTMLAttributes<HTMLDivElement>>;
 
 export interface StickyProps {
+    relative?: boolean;
     isActive?: boolean;
     className?: string;
     style?: any;
@@ -18,6 +19,7 @@ export interface StickyProps {
     bottomOffset?: number;
     onStickyStateChange?(isSticky: boolean): void;
     disableCompensation?: boolean;
+    disableHardwareAcceleration?: boolean;
 }
 
 export const Sticky: React.ComponentClass<StickyProps>;
